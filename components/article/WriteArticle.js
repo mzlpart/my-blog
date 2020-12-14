ed1467ec70a9b226bdeb13833f2b9ba0fcfaec6a
muzhlf <muzhlf@yonyou.com>
2020-12-14 14:51:53 +0800

    【文章模块】--组件初始化

---
+++ components/article/WriteArticle.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: mzl
+ * @Date: 2020-12-07 08:57:34
+ * @LastEditTime: 2020-12-07 08:57:34
+ * @Description: 写文章 bytemd 插件
+ */
+
+export default (props) => {
+    return (
+        <div>111</div>
+    )
+}
