b1a71f1fc3e8b7ae0d74031d4c4ecba44e3be989
muzhlf <muzhlf@yonyou.com>
2020-12-14 16:48:41 +0800

    【next api路由】可以有，但没必要，会破坏原本的代码存放规则，使代码变的难以维护

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+// 嗯，这个功能不错，但是使用这个的话，后端api的逻辑就被迫放到这里，跟前端代码存在位置上的冲突，感觉怪的很；后端路由什么的还是放在backend中做吧
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
