237ff0c1a1538e37ea514440e5e8252e803a3b27
mzlpart <18519271704@163.com>
2020-12-16 21:49:48 +0800

    【文章模块】--前端编辑器完善

--- pages/writeArticle.js
+++ pages/writeArticle.js
@@ -1,30 +1,52 @@
 /*
  * @Author: mzl
  * @Date: 2020-12-07 08:57:34
- * @LastEditTime: 2020-12-07 08:57:34
+ * @LastEditTime: 2020-12-16 21:44:50
  * @Description: https://github.com/kkfor/for-editor
  */
-import { useState, useEffect, useContext, useRef } from 'react';
-import dynamic from 'next/dynamic';
-const Editor = dynamic(import('for-editor'), { ssr: false });
+import { useState, useEffect, useContext, useRef } from "react";
+import { Select } from "antd";
+import dynamic from "next/dynamic";
+import "../styles/Editor.module.less";
+
+const { Option } = Select;
+const Editor = dynamic(import("for-editor"), { ssr: false });
 
 export default (props) => {
-    const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
-    function save(params) {
-        console.log('mm-params', params)
-    }
+  function save(params) {
+    console.log("mm-params", params);
+  }
 
-    return (
-        <div className="editor-style" style={{ marginTop: 60 }}>
-            <Editor
-                height="800px"
-                preview={true}
-                subfield={true}
-                value={value}
-                placeholder="可以预留一个模板位置在这里"
-                onSave={params => save(params)}
-                onChange={value => setValue(value)} />
-        </div>
-    );
-}
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+
+  return (
+    <div className="editor-container">
+      <div className="article-type">
+        <Select
+          bordered={false}
+          defaultValue="react"
+          style={{ width: 120 }}
+          onChange={handleChange}
+        >
+          <Option value="react">React</Option>
+          <Option value="js">Js</Option>
+          <Option value="node">Node</Option>
+          <Option value="other">Other</Option>
+        </Select>
+      </div>
+      <Editor
+        height="800px"
+        preview={true}
+        subfield={true}
+        value={value}
+        placeholder="可以预留一个模板位置在这里"
+        onSave={(params) => save(params)}
+        onChange={(value) => setValue(value)}
+      />
+    </div>
+  );
+};
