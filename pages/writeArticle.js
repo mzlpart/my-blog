49ea15e1da569354ea39dc8c62e54a9cc8bafbe9
muzhlf <muzhlf@yonyou.com>
2020-12-18 10:00:10 +0800

    【文章模块】--编辑器刷新报错

--- pages/writeArticle.js
+++ pages/writeArticle.js
@@ -1,14 +1,14 @@
 /*
  * @Author: mzl
  * @Date: 2020-12-07 08:57:34
- * @LastEditTime: 2020-12-18 00:36:40
+ * @LastEditTime: 2020-12-18 09:59:40
  * @Description: https://github.com/kkfor/for-editor
  */
 import { useState, useEffect, useContext, useRef } from "react";
 import { Select } from "antd";
 import dynamic from "next/dynamic";
 import { useRouter } from 'next/router'
-import MarkdownIt  from 'markdown-it';
+import MarkdownIt from 'markdown-it';
 import { CacheConfig } from '../utils';
 import "../styles/Editor.module.less";
 
@@ -18,15 +18,19 @@ const Editor = dynamic(import("for-editor"), { ssr: false });
 
 export default (props) => {
 
-  let localType = CacheConfig.getCache('articleType');
-  let localMarkdonw = CacheConfig.getCache('markdonwValue');
+  const [articleType, setArticleType] = useState("react"); // 文章类型
+  const [markdonwValue, setMarkdonwValue] = useState("");  // markdown文本
 
-  const [articleType, setArticleType] = useState(localType || "react"); // 文章类型
-  const [markdonwValue, setMarkdonwValue] = useState(localMarkdonw || "");  // markdown文本
+  // 处理未保存文章缓存
+  useEffect(() => {
+    let localType = CacheConfig.getCache('articleType') || 'react';
+    let localMarkdonw = CacheConfig.getCache('markdonwValue') || '';
+    setArticleType(localType);
+    setMarkdonwValue(localMarkdonw);
+  }, []);
 
   // 监听路由变化，提示我自己没有保存就想跳转页面
   const router = useRouter();
-
   useEffect(() => {
     const handleRouteChange = (url) => {
       console.log('App is changing to: ', url)
