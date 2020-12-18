7cac8e9291df4488d9530e3819b33d2c27a4f148
muzhlf <muzhlf@yonyou.com>
2020-12-18 10:25:56 +0800

    【Header】--更改为同步导入，并使用useEffect重构内部缓存逻辑

--- components/Header.js
+++ components/Header.js
@@ -1,29 +1,31 @@
 /*
  * @Author: your name
  * @Date: 2020-11-22 11:40:54
- * @LastEditTime: 2020-12-08 21:58:27
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-12-18 10:23:04
+ * @LastEditors: your name
  * @Description: In User Settings Edit
  * @FilePath: \blog\components\Header.js
  */
+import { useState, useEffect, useContext, useRef } from "react";
 import Link from "next/link";
 import Login from "./user/Login";
 import UserAvatar from "./user/Avatar";
 import IfComp from "if-comp";
-import { useState, useEffect, useContext, useRef } from "react";
 import Write from "./user/Write";
 import { UserContext } from "../pages/_app";
 import { CacheConfig } from '../utils';
 
 export default (props) => {
-  let { state } = useContext(UserContext);
+  let { state, dispatch } = useContext(UserContext);
   let { username, isLogin } = state;
-  let userInfo = CacheConfig.getCache('userInfo');
   // 使用缓存数据
-  if(userInfo) {
-    username = userInfo.username;
-    isLogin = userInfo.isLogin;
-  }
+  useEffect(() => {
+    let userInfo = CacheConfig.getCache('userInfo');
+    if(userInfo) {
+      dispatch({type: 'login', username: userInfo.username, isLogin: userInfo.isLogin});
+    }
+  }, []);
+
   return (
     <div className="header">
       <Link href="/">
--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-11-22 10:51:22
- * @LastEditTime: 2020-12-17 16:37:08
+ * @LastEditTime: 2020-12-18 10:22:56
  * @LastEditors: your name
  * @Description: 公共头部提取
  * @FilePath: \my-blog\components\Layout.js
  */
-import dynamic from 'next/dynamic'
-import "../styles/Layout.module.less";
+import Header from '../components/Header';
 
-// 这个有更好的方式解决：不导入这个，针对Login和Avatar进行动态导入，这样，页面显示更友好
-const Header = dynamic(import("../components/Header"),
-  {
-    ssr: false   // 禁止使用 SSR
-  }
-)
+import "../styles/Layout.module.less";
 
 export default ({ children }) => (
   <div className="container">
