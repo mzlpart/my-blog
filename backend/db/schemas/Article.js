62ec993b5d08dce7f79a6365bf01a1c9118cf6d8
muzhlf <muzhlf@yonyou.com>
2020-12-28 09:16:50 +0800

    【文章模块】--增加描述字段

--- backend/db/schemas/Article.js
+++ backend/db/schemas/Article.js
@@ -1,10 +1,14 @@
 /*
  * @Author: mzl
  * @Date: 2020-12-21 14:02:50
- * @LastEditTime: 2020-12-25 11:07:18
+ * @LastEditTime: 2020-12-28 09:16:26
  * @LastEditors: mzl
  * @Description: 文章数据结构表
+ * @param type 文章类型
+ * @param description 描述
+ * @param content 内容
  */
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -13,6 +17,10 @@ const articleSchema = new Schema({
         type: String,
         require: true
     },
+    description: {
+        type: String,
+        require: true
+    },    
     content: {
         type: String,
         require: true
